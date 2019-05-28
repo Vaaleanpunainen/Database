0f4d471918f0e47a009994689bb8a07b8cb023b0
Ewelina Backtrog <oyfg@o2.pl>
2019-05-28 15:38:21 +0200

    delete doubled line and add only one

--- script1.py
+++ script1.py
@@ -10,18 +10,12 @@ def create_table():
 def insert_values(item, quantity, price):
     conn=sqlite3.connect("lite.db")
     cur=conn.cursor()
-    # before filling database:
-    # cur.execute("INSERT INTO store VALUES ('Wine',50,25)")
-    # after filling the first line of database:
     cur.execute("INSERT INTO store VALUES (?,?,?)",(item, quantity, price))
     conn.commit()
     conn.close()
 
-# after filling the first line of database:
-# insert_values("Cofee",250,8)
-# then change into:
-# insert_values("Water",500,2.5)
-# executed two times by mistake
+#insert_values("Coffee",250,8)
+#insert_values("Water",500,2.5)
 
 def view():
     conn=sqlite3.connect("lite.db")
@@ -30,5 +24,14 @@ def view():
     rows=cur.fetchall()
     conn.close()
     return rows
+
+def delete_values(item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    conn.commit()
+    conn.close()
+
+# delete_values('Water')
 
 print(view())
